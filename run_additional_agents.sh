4775efc1c21370d9b02775f364997fd8ddde376e
Florian Treml <florian.treml@botium.at>
2019-10-04 14:30:31 +0000

    adding agent num to env

---
+++ run_additional_agents.sh
@@ -0,0 +1,5 @@
+BOTIUMBOX_AGENT_NUM=2 docker-compose run -d agent
+BOTIUMBOX_AGENT_NUM=3 docker-compose run -d agent
+BOTIUMBOX_AGENT_NUM=4 docker-compose run -d agent
+BOTIUMBOX_AGENT_NUM=5 docker-compose run -d agent
+
